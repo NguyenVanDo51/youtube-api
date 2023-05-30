3f374bd5c8d577b6f762bca8d14840dd757a25a6
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2023-05-30 13:03:53 +0700

    as

---
+++ api/youtube/views.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from youtube_transcript_api import YouTubeTranscriptApi
+
+def transcript(request):
+    video_id = request.GET.get('video_id')
+    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+
+    t = []
+    for transcript in transcript_list:
+        if transcript.language_code == 'en-GB' or transcript.language_code == 'en':
+            t = transcript.fetch()
+    return JsonResponse(t, safe=False)
